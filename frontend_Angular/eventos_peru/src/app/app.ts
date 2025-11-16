0ff52c5aa3e2dc34fa444da3e6fb20a22e5a5a55
Deyvisgova <deyvisgova@gmail.com>
2025-11-16 12:36:12 -0500

    Fix eventos table rendering

--- frontend_Angular/eventos_peru/src/app/app.ts
+++ frontend_Angular/eventos_peru/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-import { Router, RouterOutlet, RouterLink } from '@angular/router';
+import { NavigationEnd, Router, RouterOutlet, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { Navbar } from './componentes/navbar/navbar';
 import { Slider } from './componentes/slider/slider';
 import { PiePagina } from './componentes/pie-pagina/pie-pagina';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-root',
@@ -15,10 +16,21 @@ import { PiePagina } from './componentes/pie-pagina/pie-pagina';
 })
 export class App {
   rutaActual = '';
+  mostrarLayoutPublico = true;
 
   constructor(private router: Router) {
-    this.router.events.subscribe(() => {
-      this.rutaActual = this.router.url.replace('/', '');
-    });
+    this.actualizarEstadoLayout(this.router.url);
+
+    this.router.events
+      .pipe(filter((event): event is NavigationEnd => event instanceof NavigationEnd))
+      .subscribe((event) => {
+        this.actualizarEstadoLayout(event.urlAfterRedirects);
+      });
+  }
+
+  private actualizarEstadoLayout(url: string): void {
+    const ruta = url ?? '';
+    this.rutaActual = ruta.replace(/^\//, '');
+    this.mostrarLayoutPublico = !ruta.startsWith('/administrador');
   }
 }
