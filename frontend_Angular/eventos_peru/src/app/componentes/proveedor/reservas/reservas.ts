79cbceca6f8df91b6f107e5a5a9c447a4f42ab17
Deyvisgova <deyvisgova@gmail.com>
2025-11-17 18:06:01 -0500

    Add supplier panel child routes and placeholders

---
+++ frontend_Angular/eventos_peru/src/app/componentes/proveedor/reservas/reservas.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-reservas-proveedor',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './reservas.html',
+  styleUrls: ['./reservas.css'],
+})
+export class ReservasProveedor {}
---
+++ frontend_Angular/eventos_peru/src/app/componentes/proveedor/subir-logo/subir-logo.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-subir-logo',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './subir-logo.html',
+  styleUrls: ['./subir-logo.css'],
+})
+export class SubirLogo {}
